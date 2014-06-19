81228bb50b0a5cc8863a8c26864ffd0b4415f149
Alexander Sessler <alex-sessler@web.de>
2014-06-19 14:14:06 +0200

    pom + tests

--- test/service/impl/UserServiceImplTest.java
+++ test/service/impl/UserServiceImplTest.java
@@ -1,5 +1,6 @@
 package service.impl;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import service.UserService;
 
+@Ignore
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("/conf/application-context.xml")
 public class UserServiceImplTest {
